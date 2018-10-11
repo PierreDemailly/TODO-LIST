04c7bdba087b715ca7cfbd41df92ce6fcef69dd2
Demailly <pierredemailly.pro@gmail.com>
2018-10-11 17:00:42 +0200

    Fix router

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php 
+<?php
 require 'global.php';
 
-require 'controller/homeController.php';
+if(!isset($_GET['url']))
+    header('Location: '.BASE_URL.'login/');
+
+$url = $_GET['url'];
+
+if(!isOnline() && $url !== 'login' && $url !== 'register') {
+    header('Location: '.BASE_URL.'login/');
+}
+var_dump($url);
+require routing($url);
