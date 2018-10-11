839f5b335e13e9e8b0fa6ae2e8a800b32c58e155
Demailly <pierredemailly.pro@gmail.com>
2018-10-11 17:04:47 +0200

    Move [create project] statementt into model

--- controller/homeController.php
+++ controller/homeController.php
@@ -8,29 +8,24 @@ if(isset($_POST['add-project'])) {
         'date' => htmlspecialchars($_POST['project-deadline-date']),
         'time' => htmlspecialchars($_POST['project-deadline-time'])
     ];
-    
+
     if(empty($pj_name))
         $error[] = "Vous devez nommer votre projet.";
     if(strlen($pj_name) > 100)
         $error[] = "Le nom de votre projet ne doit pas dépasser 100 caractères.";
-    
+
     if(empty($pj_desc))
         $pj_desc = "Aucune description.";
     if(strlen($pj_desc) > 255)
         $error[] = "La description de votre projet ne doit pas dépasser 255 caractères.";
-    
+
     if(empty($pj_deadline['date']))
         $error[] = "Vous devez choisir une date.";
     if(empty($pj_deadline['time']))
         $error[] = "Vous devez choisir une heure.";
-    
+
     if(!isset($error)) {
-        $req = $db->prepare('INSERT INTO project (name, description, deadline) VALUES (:name, :desc, :deadline)');
-        $req->execute([
-            'name' => $pj_name,
-            'desc' => $pj_desc,
-            'deadline' => "$pj_deadline[date] $pj_deadline[time]"
-        ]);
+        createProject($pj_name, $pj_desc, "$pj_deadline[date] $pj_deadline[time]");
     }
 }
 
--- model/homeModel.php
+++ model/homeModel.php
@@ -1,9 +1,18 @@
 <?php
+function createProject($name, $desc, $dl) {
+    $db = getData();
+    $req = $db->prepare('INSERT INTO project (name, description, deadline) VALUES (:name, :desc, :deadline)');
+    $req->execute([
+        'name' => $name,
+        'desc' => $desc,
+        'deadline' => $dl
+    ]);
+}
 
 function getProjects() {
     $db = getData();
     $req = $db->query('SELECT project.id, project.name, COUNT(list.id) AS list_count
-                      FROM project 
+                      FROM project
                       LEFT JOIN list ON (list.project_id = project.id)
                       GROUP BY project.id DESC');
     return $projects = $req->fetchAll();
