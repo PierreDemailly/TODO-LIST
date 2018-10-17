17b990329a99cf27d280127fdedc757fe49df0ab
Demailly <pierredemailly.pro@gmail.com>
2018-10-17 14:38:41 +0200

    remove adding task feature from project detail page

--- controller/projectController.php
+++ controller/projectController.php
@@ -1,39 +1,26 @@
 <?php
 require 'model/projectModel.php';
 
-if(!validate($_GET['id']))
-  header('Location: '.BASE_URL.'home/');
-
-if(isset($_POST['add-list'])) {
-  $name = htmlspecialchars($_POST['list-name']);
-
-  $error[] = (empty($name)) ? "Vous devez nommer votre liste." : NULL;
-  $error[] = (strlen($name) < 3) ? "Le nom de votre liste doit faire au moins 3 caractères" : NULL;
-  $error[] = (strlen($name) > 100) ? "Le nom de votre liste doit faire maximum 100 caractères" : NULL;
-
-  $errors = getErrors($error);
-
-  if(!isset($errors)) {
-    createList($name, $_GET['id']);
-  }
+if (!validate($_GET['id'])) {
+    header('Location: ' . BASE_URL . 'home/');
 }
 
-if(isset($_POST['add-task'])) {
-  $name = htmlspecialchars($_POST['task-name']);
+if (isset($_POST['add-list'])) {
+    $name = htmlspecialchars($_POST['list-name']);
 
-  $error[] = (empty($name)) ? "Vous devez nommer votre liste." : NULL;
-  $error[] = (strlen($name) < 3) ? "Le nom de votre liste doit faire au moins 3 caractères": NULL;
-  $error[] = (strlen($name) > 100) ? "Le nom de votre liste doit faire maximum 100 caractères": NULL;
+    $error[] = (empty($name)) ? "Vous devez nommer votre liste." : null;
+    $error[] = (strlen($name) < 3) ? "Le nom de votre liste doit faire au moins 3 caractères" : null;
+    $error[] = (strlen($name) > 100) ? "Le nom de votre liste doit faire maximum 100 caractères" : null;
 
-  $errors = getErrors($error);
+    $errors = getErrors($error);
 
-  if(!isset($errors)) {
-    createTask($name, $_POST['list_id']);
-  }
+    if (!isset($errors)) {
+        createList($name, $_GET['id']);
+    }
 }
 
 $project = getProject($_GET['id']);
-$lists   = getLists($_GET['id']);
+$lists = getLists($_GET['id']);
 
 require 'tpl/header.tpl';
 require 'tpl/project.tpl';
