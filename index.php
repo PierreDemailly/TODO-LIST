13ec67e18522554f6f3e3c59cb620045b2abb8c4
Demailly <pierredemailly.pro@gmail.com>
2018-10-26 12:13:27 +0200

    Fix useless brackets

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
 require 'global.php';
 
-if(!isset($_SESSION['url']) OR $_SESSION['url'] === false) {
+if(!isset($_SESSION['url']) OR $_SESSION['url'] === false)
     checkUrl();
-} else {
+else
     $_SESSION['url'] = false;
-}
 
 if(empty($_GET['url']))
     header('Location: ' . BASE_URL . 'login/');
 
 $url = $_GET['url'];
 
-if(!isOnline() && $url !== 'login' && $url !== 'register') {
+if(!isOnline() && $url !== 'login' && $url !== 'register')
     header('Location: ' . BASE_URL . 'login/');
-}
 
 require routing($url);
